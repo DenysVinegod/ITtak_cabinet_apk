e195d76be317e67ecfcf1926ebb2ec52529d298f
Denys Vinegod <denys.vinegod@gmail.com>
2024-04-24 13:40:53 +0300

    Working WebWiev + JS Settings + Internet Permisions + Icons

--- app/src/main/java/com/ittak/cabinet/MainActivity.kt
+++ app/src/main/java/com/ittak/cabinet/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ittak.cabinet
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.ittak.cabinet.ui.theme.ITтакКабінетTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +22,8 @@ class MainActivity : ComponentActivity() {
             ITтакКабінетTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    // Greeting("Android")
+                    WebViewComponent("https://billing.ittak.net")
                 }
             }
         }
@@ -34,10 +38,34 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun WebViewComponent(url: String) {
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                settings.databaseEnabled = true
+                webViewClient = WebViewClient()
+                loadUrl(url)
+            }
+        }
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ITтакКабінетTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WebViewPreview() {
+    ITтакКабінетTheme {
+        WebViewComponent("https://billing.ittak.net")
     }
 }
